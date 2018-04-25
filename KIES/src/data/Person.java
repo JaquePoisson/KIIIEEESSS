b127a67802f27f7a7d9bb51dd37bd774dc66538c
jakobdobner <jakobdobner@kk-gym-edv1-12.gym.kk.local>
2018-04-25 15:38:08 +0200

    Update

--- KIES/src/data/Person.java
+++ KIES/src/data/Person.java
@@ -3,16 +3,33 @@ package data;
 public class Person {
 	private String vorname;
 	private String nachname;
+	private boolean female;
+	
+	@Override
+	public String toString() {
+		return (isFemale() ? "Frau" : "Mann") + " " + getVorname() + " " + getNachname();
+	}
+	
+	public boolean isFemale() {
+		return female;
+	}
+	
+	public void setFemale(boolean female) {
+		this.female = female;
+	}
 	
 	public String getVorname() {
 		return vorname;
 	}
+	
 	public void setVorname(String vorname) {
 		this.vorname = vorname;
 	}
+	
 	public String getNachname() {
 		return nachname;
 	}
+	
 	public void setNachname(String nachname) {
 		this.nachname = nachname;
 	}
